53b03a3dbe164a8751badaba34dde1586bbd3a7b
emmillyc-maker <emmilly.c@escolar.ifrn.edu.br>
2026-07-16 13:13:44 -0300

    Update Main.java

--- themis/src/main/java/br/edu/ifrn/Main.java
+++ themis/src/main/java/br/edu/ifrn/Main.java
@@ -7,26 +7,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Criando denúncia
         Denuncia denuncia = new Denuncia();
 
         denuncia.setTipo("Bullying");
         denuncia.setDescricao("Aluno sofrendo intimidação na sala.");
-        denuncia.setLocal("Sala 12");
-        denuncia.setAnonima(true);
 
-        // Criando service
         DenunciaService service = new DenunciaService();
 
-        // Executando cadastro da denúncia
-        service.cadastrarDenuncia(denuncia);
+        service.salvarNovaDenuncia(denuncia);
 
-        // Exibindo informações
         System.out.println("Tipo: " + denuncia.getTipo());
         System.out.println("Descrição: " + denuncia.getDescricao());
-        System.out.println("Local: " + denuncia.getLocal());
         System.out.println("Status: " + denuncia.getStatus());
-        System.out.println("Anônima: " + denuncia.isAnonima());
         System.out.println("Código: " + denuncia.getCodigo());
     }
 }
