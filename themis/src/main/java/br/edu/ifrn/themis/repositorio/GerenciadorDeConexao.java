b837420d92a7d04249ce7213f54d85e9635e46da
emmillyc-maker <emmilly.c@escolar.ifrn.edu.br>
2026-07-16 13:06:39 -0300

    Update GerenciadorDeConexao.java

--- themis/src/main/java/br/edu/ifrn/themis/repositorio/GerenciadorDeConexao.java
+++ themis/src/main/java/br/edu/ifrn/themis/repositorio/GerenciadorDeConexao.java
@@ -1,1 +1,16 @@
+package br.edu.ifrn.themis.repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GerenciadorDeConexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/TemisDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "@Emmycami1607";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
