8d293a46486de665bc5505888c61033664ade6d9
emmillyc-maker <emmilly.c@escolar.ifrn.edu.br>
2026-07-16 11:46:40 -0300

    Update DenunciaRepositorio.java
    implementando denuncia.repositorio

--- themis/src/main/java/br/edu/ifrn/themis/repositorio/DenunciaRepositorio.java
+++ themis/src/main/java/br/edu/ifrn/themis/repositorio/DenunciaRepositorio.java
@@ -1,1 +1,33 @@
+package br.edu.ifrn.themis.repositorio;
 
+import br.edu.ifrn.themis.modelo.Denuncia;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DenunciaRepositorio {
+
+    private Connection getConnection() throws SQLException {
+        return GerenciadorDeConexao.getConnection();
+    }
+
+    // [C] - INSERIR (INSERT)
+    public void inserir(Denuncia denuncia) {
+
+        String sql = "INSERT INTO denuncia (titulo, descricao, status) VALUES (?, ?, ?)";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, denuncia.getTitulo());
+            stmt.setString(2, denuncia.getDescricao());
+            stmt.setString(3, denuncia.getStatus());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao inserir denúncia no MySQL", e);
+        }
+    }
+}
